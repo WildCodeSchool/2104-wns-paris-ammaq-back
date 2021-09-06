f9062bc51dda38a3411d2a4cd2d1f23ef856c31d
mohamed <mohamed@tanke.fr>
2021-09-06 14:57:19 +0200

    Add input for question and quiz

---
+++ src/graphql/inputs/question.input.ts
@@ -0,0 +1,14 @@
+import { InputType, Field } from 'type-graphql';
+import { Question } from './../../entities/question.entity';
+
+@InputType()
+export default class QuestionInput implements Partial<Question>{
+  @Field(() => String)
+  question!: string;
+
+  @Field(() => String)
+  answer!: string;
+
+  @Field()
+  allChoices!: [];
+}
---
+++ src/graphql/inputs/quiz.input.ts
@@ -0,0 +1,12 @@
+import { InputType, Field } from 'type-graphql';
+import { Quiz } from './../../entities/quiz.entity';
+import Question from './../../entities/question.entity';
+
+@InputType()
+export default class QuizInput implements Partial<Quiz>{
+  @Field(() => String)
+  name!: string;
+
+  @Field(() => Array)
+  questions!: [Question]
+}
